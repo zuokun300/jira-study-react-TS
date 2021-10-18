05b2ebd9e07bb0f59b31e21cc3c66ef7258b609f
zuokun <zuokun@csii.com.cn>
2021-10-18 10:08:57 +0800

    feat(工具): 添加useMount函数，添加useDebounce函数
     - 添加useMount函数，添加useDebounce函数

--- src/util/util.js
+++ src/util/util.js
@@ -2,6 +2,8 @@
  * 目的是清空对象中没有数据的元素
  * @param obj
  */
+import { useEffect, useState } from "react";
+
 function cleanObject(obj) {
   const result = { ...obj };
   Object.keys(result).forEach((key) => {
@@ -15,4 +17,34 @@ function cleanObject(obj) {
 
 const isFalsy = (value) => (value === 0 ? false : !value);
 
-export { cleanObject, isFalsy };
+// 简单版本防抖
+// const useDebounce = function (callback, time) {
+//   let timer
+//   return function() {
+//     if (timer) {
+//       clearTimeout(time)
+//     }
+//     setTimeout(() => {
+//       callback()
+//     }, time)
+//   }
+// }
+// 写一个custom Hook防抖
+const useDebounce = function (value, delay) {
+  const [debounceValue, setDebounceValue] = useState([value]);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setDebounceValue(value), delay);
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+  return debounceValue;
+};
+
+// 写一个custom Hook挂载时候的函数，类似于vue的mounted
+const useMount = function (callback) {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+export { cleanObject, isFalsy, useMount, useDebounce };
