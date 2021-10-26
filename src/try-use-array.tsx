18cdeec7ba5385ed81d5c181e662a7a3bc55e75c
zuokun <zuokun@csii.com.cn>
2021-10-26 11:11:02 +0800

    feat: 添加作业答案
     - 添加作业答案

---
+++ src/try-use-array.tsx
@@ -0,0 +1,27 @@
+import { useArray, useMount } from "./util/util";
+
+export const TsReactTest = () => {
+  const persons: { name: string; age: number }[] = [
+    { name: "jack", age: 25 },
+    { name: "ma", age: 22 },
+  ];
+
+  const { value, clear, removeIndex, add } = useArray(persons);
+  useMount(() => {});
+  return (
+    <div>
+      <button onClick={() => add({ name: "john", age: 22 })}>add john</button>
+      <button onClick={() => removeIndex(0)}>remove 0</button>
+      <button style={{ marginBottom: "50px" }} onClick={() => clear()}>
+        clear
+      </button>
+      {value.map((person, index) => (
+        <div style={{ marginBottom: "50px" }} key={index}>
+          <span style={{ color: "red" }}>{index}</span>
+          <span>{person.name}</span>
+          <span>{person.age}</span>
+        </div>
+      ))}
+    </div>
+  );
+};
--- src/util/util.ts
+++ src/util/util.ts
@@ -4,7 +4,7 @@
  */
 import { useEffect, useState } from "react";
 
-function cleanObject(obj: object) {
+export function cleanObject(obj: object) {
   const result = { ...obj };
   Object.keys(result).forEach((key) => {
     // @ts-ignore
@@ -17,7 +17,8 @@ function cleanObject(obj: object) {
   return result;
 }
 
-const isFalsy = (value: unknown): boolean => (value === 0 ? false : !value);
+export const isFalsy = (value: unknown): boolean =>
+  value === 0 ? false : !value;
 
 // 简单版本防抖
 // const useDebounce = function (callback, time) {
@@ -32,7 +33,7 @@ const isFalsy = (value: unknown): boolean => (value === 0 ? false : !value);
 //   }
 // }
 // 写一个custom Hook防抖
-const useDebounce = <V>(value: V, delay?: number) => {
+export const useDebounce = <V>(value: V, delay?: number) => {
   const [debounceValue, setDebounceValue] = useState([value]);
 
   useEffect(() => {
@@ -44,10 +45,50 @@ const useDebounce = <V>(value: V, delay?: number) => {
 };
 
 // 写一个custom Hook挂载时候的函数，类似于vue的mounted
-const useMount = function (callback: () => void) {
+export const useMount = function (callback: () => void) {
   useEffect(() => {
     callback();
   }, []);
 };
 
-export { cleanObject, isFalsy, useMount, useDebounce };
+// 作业useArray——自己写的
+// export const useArray = <V>(arr: V[]) => {
+//   const [value, setValue] = useState(arr)
+//   const clear = () => {
+//     setValue([])
+//   }
+//   const removeIndex = (index: number) => {
+//     const arrCopy = value
+//     if (isFalsy(index) && index < arrCopy.length) {
+//       delete arrCopy[index]
+//       setValue(arrCopy)
+//     }
+//   }
+//   const add = (obj: V) => {
+//     const arrCopy = value
+//     arrCopy.push(obj)
+//     setValue(arrCopy)
+//     console.log(value);
+//   }
+//   return {value, clear, removeIndex, add }
+// }
+
+// 作业useArray——答案
+export const useArray = <T>(initialValue: T[]) => {
+  const [value, setValue] = useState(initialValue);
+  return {
+    value,
+    setValue,
+    add: (item: T) => {
+      setValue([...value, item]);
+    },
+    clear: () => {
+      setValue([]);
+    },
+    removeIndex: (index: number) => {
+      const copy = [...value];
+      copy.splice(index, 1);
+      setValue(copy);
+    },
+  };
+};
